fcd5a2166eeae5dbcf5b2e18f7ac92f8f0f62982
Ahmad El-Mowafy <ahmadelmowafy4@gmail.com>
2024-12-10 18:34:00 +0000

    Completed react-components

---
+++ react-components/my-app/src/Image.tsx
@@ -0,0 +1,9 @@
+export function Image() {
+  return (
+    <img
+      src="https://www.usatoday.com/gcdn/authoring/authoring-images/2024/07/10/USAT/74346543007-d-6-2-shrek-21.JPG?crop=2267,1274,x366,y0&width=1600&height=800&format=pjpg&auto=webp"
+      alt="Shrek"
+      className="image"
+    />
+  );
+}
